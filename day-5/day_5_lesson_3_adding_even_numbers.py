f4b65b63b1f09caf051c2dab7b1b13f0bccee282
Jose Venegas <jo.ve.or.84@gmail.com>
2024-03-03 19:51:30 -0600

    Day 5 - Lesson 3: Adding Even Numbers

---
+++ day-5/day_5_lesson_3_adding_even_numbers.py
@@ -0,0 +1,46 @@
+"""
+Instructions
+You are going to write a program that calculates the sum of all the even numbers from 1 to X. If X is 100 then the first even number would be 2 and the last one is 100:
+
+i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+
+Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+
+Also, we will constrain the inputs to only take numbers from 0 to a max of 1000.
+
+Example Input 1
+10
+Example Output 1
+30
+Example Input 2
+52
+Example Output 2
+702
+Hint
+There are quite a few ways of solving this problem, but you will need to use the range() function in any of the solutions.
+"""
+
+target = int(input()) # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+# Write your code here 👇
+
+total_sum = 0
+for num in range(2, target + 1, 2):
+  total_sum += num
+print(total_sum)
+
+
+"""
+Angela's Solutions 
+even_sum = 0
+for number in range(2, target + 1, 2):
+  even_sum += number
+print(even_sum)
+
+alternative_sum = 0
+for number in range(1, target + 1):
+  if number % 2 == 0:
+    alternative_sum += number
+print(alternative_sum)
+"""
