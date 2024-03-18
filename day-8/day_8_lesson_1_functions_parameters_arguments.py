13b7022e42f3092e94a63147ac44cc5c7574fd93
Jose Venegas <jo.ve.or.84@gmail.com>
2024-03-18 10:23:41 -0600

    Day 8 - Lesson 1: Functions, Parameters and Arguments

---
+++ day-8/day_8_lesson_1_functions_parameters_arguments.py
@@ -0,0 +1,9 @@
+# Review: 
+# Create a function called greet(). 
+def greet():
+    print("Hello!")
+    print("How are you?")
+    print("I hope everything is great!")
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+greet()
