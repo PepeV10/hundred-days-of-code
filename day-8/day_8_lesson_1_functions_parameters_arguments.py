ad91eaf80c73ba2a6f0c37318c7ea7fa3254457b
Jose Venegas <jo.ve.or.84@gmail.com>
2024-03-18 12:05:49 -0600

    Day 8 - Lesson 1: Added extra examples

--- day-8/day_8_lesson_1_functions_parameters_arguments.py
+++ day-8/day_8_lesson_1_functions_parameters_arguments.py
@@ -1,9 +1,22 @@
-# Review: 
-# Create a function called greet(). 
-def greet():
-    print("Hello!")
-    print("How are you?")
-    print("I hope everything is great!")
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-greet()
+# # Review: 
+# # Create a function called greet(). 
+# def greet():
+#     print("Hello!")
+#     print("How are you?")
+#     print("I hope everything is great!")
+# # Write 3 print statements inside the function.
+# # Call the greet() function and run your code.
+# greet()
+
+# #Function that allows for input
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+# greet_with_name("Anna")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+greet_with(name="Anna", location="Russia")
+    
