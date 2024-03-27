3ccc844d6a37453d0ae0d57d28cf5c9ad4cb5835
Jose Venegas <jo.ve.or.84@gmail.com>
2024-03-27 15:48:09 -0600

    Day 1 - Project: Band Name Generator - Added New Features

--- day-1/day_1_project_band_name_generator.py
+++ day-1/day_1_project_band_name_generator.py
@@ -1,3 +1,4 @@
+"""
 # 1. Create a greeting for your program.
 greeting = "Welcome to Band Name Generator, We'll help you pick the best name for your up and coming Band!\n"
 print(greeting)
@@ -10,3 +11,17 @@ band_name = f"Your band name could be: The {city_name} {pet_name}(s)!!!\n"
 # 5. Make sure the input cursor shows on a new line:
 print(band_name)
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
+"""
+print("Welcome to Band Name Generator. We'll help you pick the best name for your up and coming Band!\n")
+print("This program helps you generate a unique band name based on a few of your personal preferences.")
+
+while True:
+    city_name = input("What city did you grow up in?\n").title()
+    pet_name = input("What is the name of your pet?\n").title()
+    band_name = f"Your band name could be: The {city_name} {pet_name}(s)!!!"
+    print(band_name)
+
+    continue_generation = input("Do you want to generate another Band Name? (Yes/No)\n")
+    if continue_generation.lower() == "no":
+        print("Thank you for using the Band Name Generator. Enjoy your brand new Band Name. Have a great day!")
+        break
